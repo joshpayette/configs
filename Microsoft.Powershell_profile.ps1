7ac880e3ab30b3adec46c4d79da5babcc9da896d
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2025-04-23 11:52:40 -0400

    Update Microsoft.Powershell_profile.ps1

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,3 +1,5 @@
 # Found at C:\Users\joshw\Dropbox\Documents\WindowsPowerShell\
+# Confirm this location by typing $PROFILE
+# Reload profile with . $PROFILE
 Invoke-Expression (&starship init powershell)
 Set-Location C:\.Coding
