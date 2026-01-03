af38b229ddf51ddbf819384e67d2239f410fbd99
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2026-01-03 10:17:16 -0500

    Delete Microsoft.Powershell_profile.ps1

--- Microsoft.Powershell_profile.ps1
+++
@@ -1,5 +0,0 @@
-# Found at C:\Users\joshw\Dropbox\Documents\WindowsPowerShell\
-# Confirm this location by typing $PROFILE
-# Reload profile with . $PROFILE
-Invoke-Expression (&starship init powershell)
-Set-Location C:\.Coding
